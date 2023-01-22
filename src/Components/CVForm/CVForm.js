398c7781282a9c30f1385a81c48c94a50763c74d
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-01-22 12:59:54 +0100

    Import experience component

--- src/Components/CVForm/CVForm.js
+++ src/Components/CVForm/CVForm.js
@@ -1,10 +1,12 @@
 import PersonalInfo from "./PersonalInfo";
 import '../../Styles/CVForm/CVForm.css';
+import Experience from './Experience';
 
 const CVForm =(props)=>{
     return(
         <form id='CVForm' className='CV'>
             <PersonalInfo personalInfo={props.cv.personalInformation} updatePersonalInfo={props.getPersonalInformation}/>
+            <Experience experience={props.cv.experience} addExperience={props.addExperience}/>
         </form>
     )
 }
