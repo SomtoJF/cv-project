0f52c7a456b38f15cce6932b63da11aa48209d39
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-01-26 16:19:55 +0100

    Import work experience component

--- src/Components/CVPreview/CVPreview.js
+++ src/Components/CVPreview/CVPreview.js
@@ -1,13 +1,15 @@
 import PersonalInformation from './PersonalInformation';
+import WorkExperience from './WorkExperience'
 import '../../Styles/CVPreview/CVPreview.css';
 
 const CVPreview = (props)=>{
     const personalInformation = props.state.personalInformation;
+    const experience  = props.state.experience;
 
     return(
         <div id='preview' className='CV'>
             <PersonalInformation information={personalInformation} />
-            {/* <WorkExperience /> */}
+            <WorkExperience experience={experience} />
             {/* <EducationHistory /> */}
         </div>
     )
