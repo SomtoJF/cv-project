1f1108c140a7edd435256bed21092dc8e201a2b3
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-01-27 16:42:44 +0100

    Import Educational History module

--- src/Components/CVPreview/CVPreview.js
+++ src/Components/CVPreview/CVPreview.js
@@ -1,16 +1,18 @@
 import PersonalInformation from './PersonalInformation';
-import WorkExperience from './WorkExperience'
+import WorkExperience from './WorkExperience';
+import EducationHistory from './EducationHistory';
 import '../../Styles/CVPreview/CVPreview.css';
 
 const CVPreview = (props)=>{
     const personalInformation = props.state.personalInformation;
     const experience  = props.state.experience;
+    const education = props.state.education;
 
     return(
         <div id='preview' className='CV'>
             <PersonalInformation information={personalInformation} />
             <WorkExperience experience={experience} />
-            {/* <EducationHistory /> */}
+            <EducationHistory education={education}/>
         </div>
     )
 }
