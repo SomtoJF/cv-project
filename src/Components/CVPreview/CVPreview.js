8ef2720c8b21665b526759705ca893d60a272899
Somtochukwu Francis <somtofrancis5@gmail.com>
2023-01-28 13:46:29 +0100

    Import Skills component

--- src/Components/CVPreview/CVPreview.js
+++ src/Components/CVPreview/CVPreview.js
@@ -1,18 +1,21 @@
 import PersonalInformation from './PersonalInformation';
 import WorkExperience from './WorkExperience';
 import EducationHistory from './EducationHistory';
+import MySkills from './MySkills';
 import '../../Styles/CVPreview/CVPreview.css';
 
 const CVPreview = (props)=>{
     const personalInformation = props.state.personalInformation;
     const experience  = props.state.experience;
     const education = props.state.education;
+    const skills = props.state.skills;
 
     return(
         <div id='preview' className='CV'>
             <PersonalInformation information={personalInformation} />
             <WorkExperience experience={experience} />
             <EducationHistory education={education}/>
+            <MySkills skills={skills} />
         </div>
     )
 }
